cb02dcf34b78b2471d7fa11f626de64e79047ba3
ChathuraSam <chathura.samarajeewa@gmail.com>
2019-04-18 14:58:57 +0530

    added a login page php

---
+++ login.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Helping Friens</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="vavuniya campus pastpaper">
+        <meta name="author" content="lakshan sampath">
+
+        <!--link css and js (bootsrap,jqury)-->
+
+        <!--BOOTSTRAP-->
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+        <title>Helping Friens</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+
+  <link href="https://fonts.googleapis.com/css?family=Pacifico&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+  
+  <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+
+
+
+    </head>
+
+    <body>
+            <!--navigartion-->
+            <div class="naaa">
+            <nav class="navbar navbar-expand-lg navbar-light navbar-color">
+              <a class="logo"><img class="logo1 " src="image/2.png"></a>
+                <a class="navbar-brand" href="#"><p class="faculty"> Faculty Of Appllied Sceince<br> University OF Jaffna</p></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#level1">Level 1</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#level2">Level 2</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#level3">Level 3</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#level4">Level 4</a>
+                    </li>
+                    
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>
+
+            </div>
+           
+              <div >
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+        
+    </body>
+</html>
